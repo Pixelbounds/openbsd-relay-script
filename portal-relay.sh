73e7128e9bca6c807f2469439c8952cbff5059bd
Dennis <nocraze@protonmail.com>
2023-04-12 17:59:15 +0200

    First step done, and removed some previous bad code

--- portal-relay.sh
+++ portal-relay.sh
@@ -1,13 +1,19 @@
 #! /usr/local/bin/bash
-echo "Do you want to start Portal relay on port:20371? (1 for yes, 0 for no)"
+if [ ps | grep '.portal' -eq true ]
+	then
+		echo "You are currently running a Portal relay:" && ps | grep "./portal"
+	else
+		echo "No relay detected in system processes, continuing..."
+fi
+
+
+echo "Would you like to set up a Portal relay? (1 for yes, 0 for no)"
 read relayconnection
 
 if [ $relayconnection -eq 1 ]
-then
-	cd /home/dennis/portal/
-	pwd
-	nohup ./portal serve --port 20371&
-	echo "Relay is up on port 20371 and running in background"
+	then
+	cat /home/$USER/.config/portal/config.yml
+	
 fi
 
 if [ $relayconnection -eq 0 ]
@@ -15,3 +21,4 @@ then
 	echo "User aborted script, closing..."
 fi
 
+# PID for portal serve is 37225
