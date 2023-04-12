39c841f0b64236f230c2f661768fd3df27f9b1a9
Dennis <nocraze@protonmail.com>
2023-04-13 00:56:00 +0200

    First trial run on OpenBSD, remember to change line 1 to match OpenBSD formatting of bash

--- portal-relay.sh
+++ portal-relay.sh
@@ -1,24 +1,61 @@
-#! /usr/local/bin/bash
-if [ ps | grep '.portal' -eq true ]
+#! /bin/bash
+relayrunning=$(ps | grep './portal')
+relayport=$(sed -n 3p /home/$USER/.config/portal/config.yml | grep -Eo '[0-7]{0,4}[0-9]{0,4}|65535')
+
+if [ $relayrunning ]
 	then
-		echo "You are currently running a Portal relay:" && ps | grep "./portal"
+		echo "You are currently running a Portal relay: $relayrunning"
 	else
 		echo "No relay detected in system processes, continuing..."
 fi
-
-
 echo "Would you like to set up a Portal relay? (1 for yes, 0 for no)"
 read relayconnection
-
 if [ $relayconnection -eq 1 ]
 	then
-	cat /home/$USER/.config/portal/config.yml
-	
+	echo "Current configured port is $relayport"
+	echo "Would you like to change the port of the relay? (1 for yes, 0 for no)"
+	read relayconfig
+	if [ $relayconfig -eq 1 ]
+		then
+		echo "Please select between 1024 - 65535 (Don't select an already open port)"
+		read portselection
+		while [[ -z $portselection ]]
+			do
+			echo "Input empty select a port between 1024 - 65535"
+			read portselection
+			if [[ $portselection -gt 65536 ]]
+				then 
+				echo "Port selection is greater than 65535, select a port between 1024 - 65535"
+				read portselection
+			fi
+		done
+		while [[ $portselection -gt 65536 ]]
+			do
+			echo "Input is greater than 65535, please select a port between 1024 - 65535"
+			read portselection
+			if [[ -z $portselection ]]
+				then
+				echo "Input empty select a port between 1024 - 65535"
+				read portselection
+			fi
+		done
+		sed -i "3s/.*/port_relay_serve: $portselection/" "/home/$USER/.config/portal/config.yml"
+		echo "Port $portselection configured for Portal"
+		echo "Would you like to run the relay on port $portselection?"
+		read startrelay
+			if [ $startrelay -eq 1 ]
+				then
+				cd /home/$USER/portal/
+				nohup ./portal serve --port $relayport&
+				echo "$relayrunning"
+			else
+				echo "Invalid input or user aborted script, closing."
+			fi
+	else
+		echo "Portal relay is running on port $relayport"
+	fi
 fi
-
 if [ $relayconnection -eq 0 ]
 then
 	echo "User aborted script, closing..."
 fi
-
-# PID for portal serve is 37225
