9f7a43fa7463c9b5e36101ad68bb539b94e77318
Dennis <nocraze@protonmail.com>
2023-04-13 13:34:49 +0200

    Script worked finally. Fixed spelling and added a disclaimer if a relay is already found running.

--- portal-relay/portal-relay.sh
+++ portal-relay/portal-relay.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# Function to check if the relay is running
 is_relay_running() {
     relayrunning=$(ps auxwww | grep './portal' | grep -v grep)
     if [ -n "$relayrunning" ]; then
-        echo "You are currently running a Portal relay. $relayrunning"
+        echo "You are currently running a Portal relay. $relayrunning. After the script has finished configuring Portal remember to close processes running on a different port"
         return 0
     else
         echo "No relay detected in system processes, continuing..."
@@ -12,14 +11,13 @@ is_relay_running() {
     fi
 }
 
-# Check if the relay is already running
 is_relay_running
 
 echo "Would you like to set up a Portal relay? (1 for yes, 0 for no)"
 read relayconnection
 
 if [ "$relayconnection" -eq 1 ]; then
-    echo "Would you like to change the port of the relay,d efault port is 8080? (1 for yes, 0 for no)"
+    echo "Would you like to change the port of the relay, default port is 8080? (1 for yes, 0 for no)"
     read relayconfig
     if [ "$relayconfig" -eq 1 ]; then
         echo "Please select between 1024 - 65535 (Don't select an already open port)"
